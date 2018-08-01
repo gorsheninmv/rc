f09d7689c96874692e71b3956e9bfde3c36be1af
Misha Gorshenin <gorshenin.mv@gmail.com>
2018-08-01 23:58:40 +0500

    Added tmux symlink script

--- symlinks_scripts/tmux.sh
+++ symlinks_scripts/tmux.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
 SCRIPT=$(realpath -e "$0")
-BASEDIR=$(dirname "$SCRIPT")
+BASEDIR=$(dirname "$(dirname "$SCRIPT")")
+
+FILENAME=".tmux.conf"
+LINKPATH="$HOME/$FILENAME"
+FILEPATH="$BASEDIR/$FILENAME"
+
+if [ -f "$LINKPATH" ]
+	then rm "$LINKPATH"
+fi
+ln -s "$FILEPATH" "$LINKPATH"
+
+
+FILENAME=".tmux.conf.local"
+LINKPATH="$HOME/$FILENAME"
+FILEPATH="$BASEDIR/$FILENAME"
+
+
+if [ -f "$LINKPATH" ]
+	then rm "$LINKPATH"
+fi
+ln -s "$FILEPATH" "$LINKPATH"
+
+
