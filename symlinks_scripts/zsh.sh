2939f444eebe16c1973f4a7dea25cf5b2041c4e0
Misha Gorshenin <gorshenin.mv@gmail.com>
2019-05-19 14:14:30 +0500

    Added LANG settings

--- symlinks_scripts/zsh.sh
+++ symlinks_scripts/zsh.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 echo "source ~/.bash_aliases" >> ~/.zshrc
+echo 'export LANG="en_US.UTF-8"' >> ~/.zshrc
+echo 'export LC_ALL="en_US.UTF-8"' >> ~/.zshrc
 
 
 SCRIPT=$(realpath -e "$0")
