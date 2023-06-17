62766b56778a63c13452cb84c484b4e94f2af906
Misha Gorshenin <gorshenin.mv@gmail.com>
2023-06-17 21:48:08 +0500

    change font

--- wezterm/src/wezterm.lua
+++ wezterm/src/wezterm.lua
@@ -1,7 +1,7 @@
 local wz = require 'wezterm'
 
 return {
-  font = wz.font 'Fira Code',
+  font = wz.font 'FiraCode Nerd Font Mono',
   font_size = 11,
   color_scheme = "PaperColorLight (Gogh)",
   enable_tab_bar = false,
