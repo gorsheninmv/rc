98f633693d97f09dc2cd0dc63a12f22088d76868
Горшенин Михаил (Gorshenin_MV) <Gorshenin_MV@directum.ru>
2024-02-19 00:06:05 +0500

    sync nvim tree with cwd

--- neovim/nvim/lua/config/nvim-tree/init.lua
+++ neovim/nvim/lua/config/nvim-tree/init.lua
@@ -9,9 +9,16 @@ local function on_attach(bufnr)
   vim.keymap.set('n', 'u', api.tree.change_root_to_parent, opts('Up'))
 end
 
+vim.g.nvim_tree_respect_buf_cwd = 1
+
 require('nvim-tree').setup({
   on_attach = on_attach,
-  sync_root_with_cwd = true
+  sync_root_with_cwd = true,
+  update_cwd = true,
+  update_focused_file = {
+      enable = true,
+      update_cwd = true,
+  }
 })
 
 vim.keymap.set('n', '<leader>nt', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
