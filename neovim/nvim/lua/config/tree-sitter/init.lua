069f06b87e1712fa9a9e13f48301b6f2315c32e4
Misha Gorshenin <gorshenin.mv@gmail.com>
2024-09-29 13:34:52 +0500

    add new parsers

--- neovim/nvim/lua/config/tree-sitter/init.lua
+++ neovim/nvim/lua/config/tree-sitter/init.lua
@@ -4,9 +4,15 @@ tree_sitter.setup {
   ensure_installed = {
     'html',
     'css',
+
     'lua',
+    'vim',
+
     'markdown',
     'markdown_inline',
+
+    'luadoc',
+    'vimdoc',
   },
   highlight = {
     enable = true,
