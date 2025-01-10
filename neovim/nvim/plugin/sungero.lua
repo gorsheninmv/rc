3b99d068d9580dd96955d6a3f33678d564ec73b2
Горшенин Михаил (Gorshenin_MV) <Gorshenin_MV@directum.ru>
2025-01-10 12:11:34 +0500

    fix sungero plugin

--- neovim/nvim/plugin/sungero.lua
+++ neovim/nvim/plugin/sungero.lua
@@ -1,7 +1,7 @@
 local createcmd = vim.api.nvim_create_user_command
 
 createcmd("SungeroOpenConfigYml", function ()
-  vim.cmd.edit("file:///D:/Projects/DirectumLauncher/etc/config.yml")
+  vim.cmd.edit(vim.fs.normalize("d:/Projects/DirectumLauncher/etc/config.yml"))
 end, {})
 
 createcmd("SungeroFormatJson", function ()
