89fa9cd8a742d39a2901eaa27db7f39502604972
Misha Gorshenin <gorshenin.mv@gmail.com>
2025-08-30 16:45:16 +0500

    add mini notifier

--- neovim/nvim/lua/config/plugins/mini.lua
+++ neovim/nvim/lua/config/plugins/mini.lua
@@ -4,6 +4,14 @@ return {
     config = function()
       local statusline = require("mini.statusline")
       statusline.setup { use_icons = true }
+      local notify = require('mini.notify')
+      notify.setup()
+
+      local opts = nil
+      if vim.o.verbose > 0 then
+        opts = { DEBUG = { duration = 3000 } }
+      end
+      vim.notify = notify.make_notify(opts)
     end,
   }
 }
