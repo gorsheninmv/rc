c47ec2a914623b19be61f1195a330b1dbf2aa802
Misha Gorshenin <gorshenin.mv@gmail.com>
2025-10-29 23:33:12 +0500

    use native lsp formatter for golang

--- neovim/nvim/after/ftplugin/go.lua
+++ neovim/nvim/after/ftplugin/go.lua
@@ -4,9 +4,10 @@ vim.opt_local.shiftwidth = 0 -- when zero the 'tabstop' value will be used
 vim.opt_local.foldmarker = "// #region,// #endregion"
 vim.opt_local.foldmethod = "marker"
 
-vim.keymap.set("n", "<localleader>ta", function ()
+vim.keymap.set("n", "<localleader>ta", function()
   local winid = vim.api.nvim_get_current_win()
   local cmd = "gotestsum --format testname -- -fullpath ./..."
   print("test")
   require("bterm"):exec(winid, cmd)
 end, { buffer = true, noremap = true, silent = true })
+
