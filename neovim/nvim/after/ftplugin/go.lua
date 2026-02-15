55d15c7142a258c78ab293a5f486020662b9c237
Misha Gorshenin <gorshenin.mv@gmail.com>
2026-02-15 15:08:12 +0300

    add new ways to run tests

--- neovim/nvim/after/ftplugin/go.lua
+++ neovim/nvim/after/ftplugin/go.lua
@@ -6,11 +6,15 @@ vim.opt_local.foldmethod = "marker"
 
 vim.treesitter.start()
 
-vim.keymap.set("n", "<localleader>ta", function()
-  local winid = vim.api.nvim_get_current_win()
-  local cmd = "gotestsum --format testname -- -tags=integration -fullpath ./..."
-  require("bterm"):exec(winid, cmd)
-end, { buffer = true, noremap = true, silent = true })
+vim.keymap.set(
+  "n", "<localleader>ta",
+  function()
+    local winid = vim.api.nvim_get_current_win()
+    local cmd = "gotestsum --format testname -- -tags=integration -fullpath ./..."
+    require("bterm"):exec(winid, cmd)
+  end,
+  { buffer = true, noremap = true, silent = true }
+)
 
 vim.api.nvim_buf_create_user_command(
   0,
@@ -18,3 +22,28 @@ vim.api.nvim_buf_create_user_command(
   function() require("neotest").run.run({strategy = "dap"}) end,
   {}
 )
+
+vim.keymap.set(
+  "n", "<localleader>t",
+  require("neotest").run.run,
+  {
+    buffer = true,
+    noremap = true,
+    silent = true,
+    desc = "run tests with Neotest",
+  }
+)
+
+vim.keymap.set(
+  "n", "<localleader>td",
+  function ()
+    require("neotest").run.run({strategy = "dap"})
+  end,
+  {
+    buffer = true,
+    noremap = true,
+    silent = true,
+    desc = "debug test with Neotest",
+  }
+)
+
