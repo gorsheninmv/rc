b410b2852d2be7e72ebea9c1e20484a25b60310d
Misha Gorshenin <gorshenin.mv@gmail.com>
2026-04-12 23:46:04 +0500

    add autocmd for no usages

--- neovim/nvim/after/plugin/go.lua
+++ neovim/nvim/after/plugin/go.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
   group = g,
   pattern = "*.go",
   callback = function(args)
-    vim.api.nvim_create_autocmd('BufWritePre', {
+    vim.api.nvim_create_autocmd("BufWritePre", {
       group = g,
       buffer = args.buf,
       callback = function()
@@ -25,3 +25,82 @@ vim.api.nvim_create_autocmd("LspAttach", {
     )
   end
 })
+
+local ns = vim.api.nvim_create_namespace("no-usages")
+local function append_virtual_text(bufnr, line, start, stop, source)
+  vim.diagnostic.set(ns, 0, {{
+    bufnr = bufnr,
+    lnum = line, -- line number (0-indexed)
+    col = start,
+    end_col = stop,
+    severity = vim.diagnostic.severity.WARN,
+    message = "no usages",
+    source = source,
+  }})
+end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = g,
+  pattern = "*.go",
+  callback = function(args)
+
+    local function analyze(ev)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        -- skip unsupported clients
+        if not client:supports_method("textDocument/documentSymbol") then
+          return
+        end
+
+        vim.lsp.buf_request(ev.buf, "textDocument/documentSymbol", {
+          textDocument = vim.lsp.util.make_text_document_params(),
+        }, function(_, result, _, _)
+          if not result then return end
+
+          vim.api.nvim_buf_clear_namespace(ev.buf, ns, 0, -1)
+
+          local function process(symbols)
+            for _, sym in ipairs(symbols) do
+              if vim.tbl_contains({ 5, 6, 8, 12, 13, 14, 23, }, sym.kind) then
+                local pos = sym.selectionRange.start
+                local line = pos.line
+
+                local params = {
+                  textDocument = vim.lsp.util.make_text_document_params(),
+                  position = pos,
+                  context = { includeDeclaration = true },
+                }
+
+                client:request("textDocument/references", params, function(_, refs)
+                  if not refs or #refs == 0 then return end
+                  vim.schedule(function()
+                    local refsCount = #refs - 1
+
+                    if refsCount == 0 then
+                      local start = sym.selectionRange.start.character
+                      local stop = sym.selectionRange["end"].character
+                      vim.notify(vim.inspect(sym.selectionRange))
+                      append_virtual_text(ev.buf, line, start, stop, ev.file)
+                    end
+                  end)
+                end, ev.buf)
+              end
+              if sym.children then
+                process(sym.children)
+              end
+            end
+          end
+
+          process(result)
+        end)
+    end
+    analyze(args)
+    vim.api.nvim_create_autocmd({"BufWritePost", "BufEnter"}, {
+      group = g,
+      buffer = args.buf,
+      callback = function(ev)
+        analyze(ev)
+      end
+    })
+  end
+}
+)
