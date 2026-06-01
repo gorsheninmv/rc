6ecfe443e2b330cdf85937439281a13c8214342e
Misha Gorshenin <gorshenin.mv@gmail.com>
2026-06-01 10:51:14 +0500

    nvim: update diffview

--- neovim/nvim/lua/config/plugins/diffview.lua
+++ neovim/nvim/lua/config/plugins/diffview.lua
@@ -1,3 +1,20 @@
 return {
-  "sindrets/diffview.nvim",
+  "dlyongemallo/diffview.nvim",
+  version = "*",
+  cmd = "DiffviewOpen", -- test
+  config = function()
+    require("diffview").setup({
+      persist_selections = { enabled = true },
+      enhanced_diff_hl = true,
+      use_icons = true,
+      view = {
+        default = { layout = "diff2_horizontal" },
+        merge_tool = { layout = "diff3_horizontal" },
+      },
+      file_panel = {
+        listing_style = "tree",
+        win_config = { position = "left", width = 35 },
+      },
+    })
+  end,
 }
